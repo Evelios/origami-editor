8e8ddffffaa4c1cefe2dd4de0aeba6b50bb90fe3
twaters <twaters@marvell.com>
2021-03-18 10:55:32 -0400

    Basic Fold structure

---
+++ Fold/Fold.fs
@@ -0,0 +1,20 @@
+﻿namespace Fold
+
+open FSharp.Json
+
+type Version = { major: int; minor: int; patch: int }
+
+type File =
+    { spec: Version Option
+      creator: string
+      author: string
+      title: string
+      description: string }
+
+type Fold = { file: File }
+
+module Fold =
+
+    let toJson (fold: Fold) = Json.serialize fold
+
+    let fromJson = Json.deserialize<Fold>
