44616ce49d16deb402c821cab49dd9948e1f42b9
twaters <twaters@marvell.com>
2021-04-05 20:17:00 -0400

    Updated color scheme and menu icons

--- GuiLibrary/ShrinkExpand.fs
+++ GuiLibrary/ShrinkExpand.fs
@@ -5,22 +5,8 @@ open Godot
 type ShrinkMaximizeFs() as this =
     inherit TextureButton()
 
-    let icon =
-        {| maximize = GD.Load("res://Assets/Icons/maximize.svg") :?> Texture
-           shrink = GD.Load("res://Assets/Icons/shrink.svg") :?> Texture |}
-
-    let setTexture() =
-        this.TextureNormal <-
-            match OS.WindowMaximized with
-            | true -> icon.shrink
-            | false -> icon.maximize
-
-    override this._Ready() =
-        setTexture()
-
     override this._Pressed() =
         if OS.WindowMaximized then
             OS.WindowMaximized <- false
         else
             OS.WindowMaximized <- true
-        setTexture()
